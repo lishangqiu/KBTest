f5f05efc6a598267af0ab8e64c663731ee02b656
lishangqiu <lishangqiu0930@gmail.com>
2022-02-11 20:30:07 -0800

    asdf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import countries from './countries';
+import countryNames from './countryNames';
 import $ from "jquery";
 const normalColor = "#c23f3f";
 const hoverColor = "#000";
+const oceanColor = "#ADD8E6";
+const hoverOffsetX = 10;
+const hoverOffsetY = 10;
 var currCountry;
 $(function () {
-    $("#world_map").append('<svg xmlns="http://www.w3.org/2000/svg" width="1000px" viewBox="0 0 2000 1001" style="max-width:100%;">');
-    $("svg").append(`<rect width="100%" height="100%" fill="#009dc4"/>`);
+    $("#world_map").append('<svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 2000 1001" style="max-height:100%;">');
+    $("svg").append(`<rect width="100%" height="100%" fill="${oceanColor}"/>`);
     countries.map((i) => {
         $("svg").append(`<path style="fill:${normalColor};fill-rule:evenodd;" stroke-width: "1" stroke="#ffffff" key="${i.id}" d="${i.shape}"></path>`);
         $("#world_map").html($("#world_map").html());
@@ -14,14 +18,18 @@ $(function () {
     $("path").on('mouseover', (e) => {
         e.target.style.fill = hoverColor;
         currCountry = e.target;
-        $("#countryTime p").text(currCountry.getAttribute("key"));
+        $("#countryTime p").text(countryNames[currCountry.getAttribute("key")]);
     });
-    $("path").on('mouseout', (e) => { console.log("adsfhkj"); e.target.style.fill = normalColor; currCountry = null; });
+    $("path").on('mouseout', (e) => {
+        e.target.style.fill = normalColor;
+        currCountry = null;
+    });
     // Make label follow mouse
     $(document).on('mousemove', (e) => {
         if (currCountry != null) {
             $("#countryTime").show();
-            $("#countryTime").css({ left: e.pageX, top: e.pageY });
+            $("#countryTime").css({ left: e.pageX + hoverOffsetX, top: e.pageY + hoverOffsetY });
+            $("#countryTime p").html();
         }
         else {
             $("#countryTime").hide();
